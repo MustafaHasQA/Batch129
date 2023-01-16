01e9cebeaadf0892656ebb256ebaf8a0bf1b4a84
MustafaHasQA <121973821+MustafaHasQA@users.noreply.github.com>
2023-01-16 17:55:34 +0300

    Update Hello.java

--- src/main/java/C01_Tekrar/Hello.java
+++ src/main/java/C01_Tekrar/Hello.java
@@ -3,5 +3,6 @@ package C01_Tekrar;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Locale'den Hello");
+        System.out.println("Uzaktan Hello");
     }
 }
